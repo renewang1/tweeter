abd3e45abe0f523f4ba50097f84d5272e7ca689a
Rene Wang <rene.wang1@hotmail.com>
2020-12-16 02:25:50 +0000

    char counter functional

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
   // --- our code goes here ---
   console.log('hello there')
+  $('#tweet-text').on('input', function () {
+    $(this).siblings().children('.counter').get(0).value = 140 - this.value.length;
+  });
 });
+
