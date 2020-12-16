006e3ed48cabc5ebcc9485e2644bce842f7b304a
Rene Wang <rene.wang1@hotmail.com>
2020-12-16 02:53:53 +0000

    added functionality for counter tochange color when below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   // --- our code goes here ---
   console.log('hello there')
   $('#tweet-text').on('input', function () {
+    let counterValue = $(this).siblings().children('.counter').get(0).value
     $(this).siblings().children('.counter').get(0).value = 140 - this.value.length;
+    if (counterValue <= -1) {
+      $(this).siblings().children('.counter').css("color", "yellow");
+    } else if (counterValue >= 0) {
+      $(this).siblings().children('.counter').css("color", "black");
+    }
   });
 });
 
