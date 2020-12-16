0639c12ae81a9860966923adf7be6757f06acf6d
Rene Wang <rene.wang1@hotmail.com>
2020-12-16 18:03:00 +0000

    fixed bug in code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
   // --- our code goes here ---
   console.log('hello there')
   $('#tweet-text').on('input', function () {
-    let counterValue = $(this).siblings().children('.counter').get(0).value
-    $(this).siblings().children('.counter').get(0).value = 140 - this.value.length;
-    if (counterValue <= -1) {
-      $(this).siblings().children('.counter').css("color", "yellow");
-    } else if (counterValue >= 0) {
-      $(this).siblings().children('.counter').css("color", "black");
+    // let counterValue = $(this).siblings().children('.counter').get(0).value - 1;
+    // console.log(counterValue)
+    // $(this).siblings().children('.counter').get(0).value = 140 - this.value.length;
+
+    const charLength = $(this).val().length;
+    const $tweetForm = $(this).closest("form");
+    const $counter = $tweetForm.find(".counter");
+    $counter.html(140 - charLength);
+    if (charLength > 140) {
+      $counter.css("color", "red");
+    } else {
+      $counter.css("color", "black");
     }
   });
 });
