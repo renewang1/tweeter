8fe956a6ce8657a510f59c030093c77c8bc6e4cb
Rene Wang <rene.wang1@hotmail.com>
2020-12-17 03:05:24 +0000

    completed AJAX form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,20 +13,42 @@ $(document).ready(function() {
     $('#sign').hide();
   })
 
-  renderTweets(tweetData);
+  renderTweets(data);
+
+  $("#tweet-form").on("submit", function(event) {
+    event.preventDefault();
+    const content = $("#tweet-form").serialize();
+    $("#tweet-text").empty();
+    $.post("http://localhost:8080/tweets", content, function(response) {
+      console.log('success')
+    })
+  })
 })
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const createTweetElement = function (tweet) {
   const $tweet = $(`
@@ -50,9 +72,9 @@ const createTweetElement = function (tweet) {
   return $tweet;
 }
 
-const renderTweets = function(tweetData) {
+const renderTweets = function(data) {
   $('#tweets-container').empty();
-  for (let tweet of tweetData) {
+  for (let tweet of data) {
     let $tweet = createTweetElement(tweet);
     $('#tweets-container').append($tweet);
   }
