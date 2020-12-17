a0c89688392cf30c2956f1062231851203244673
Rene Wang <rene.wang1@hotmail.com>
2020-12-17 21:49:51 +0000

    fixed bug that would keep text in textBox after user submitted tweet and removed old commented code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
       $("#error-message").hide("slow");
       $.post("http://localhost:8080/tweets", content, function(response) {
         console.log('success');
-        $("#tweet-text").empty();
+        $("#tweet-text").val("");
         loadTweets();
       })
     }
@@ -48,31 +48,6 @@ $(document).ready(function() {
 
   loadTweets();
 })
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 
 const escape =  function(str) {
   let div = document.createElement('div');
