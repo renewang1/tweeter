0923766591fa7ea21ebd0c1bbd782f7cef78db76
Rene Wang <rene.wang1@hotmail.com>
2020-12-17 03:38:46 +0000

    completed loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,8 +13,6 @@ $(document).ready(function() {
     $('#sign').hide();
   })
 
-  renderTweets(data);
-
   $("#tweet-form").on("submit", function(event) {
     event.preventDefault();
     const content = $("#tweet-form").serialize();
@@ -23,32 +21,41 @@ $(document).ready(function() {
       console.log('success')
     })
   })
+
+  const loadTweets = function () {
+    $.ajax("http://localhost:8080/tweets", { method: 'GET' })
+    .done(function(data) {
+      renderTweets(data)
+    })
+  }
+
+  loadTweets();
 })
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const createTweetElement = function (tweet) {
   const $tweet = $(`
