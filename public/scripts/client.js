619badda9ad3d8ea2e014b9e795a0a7dc924dbb8
Rene Wang <rene.wang1@hotmail.com>
2020-12-17 00:47:19 +0000

    created create tweet element and render tweets functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,4 +12,48 @@ $(document).ready(function() {
   $(".tweet-container").on('mouseout', function() {
     $('#sign').hide();
   })
+
+  renderTweets(tweetData);
 })
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function (tweet) {
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div>
+          ${tweet.user.avatars}
+          ${tweet.user.name}
+        </div>
+        <div id="sign">
+          ${tweet.user.handle}
+        </div>
+      </header>
+      <div>
+        ${tweet.content.text}
+      </div>
+      <footer>
+        ${tweet.created_at}
+      </footer>
+    </article>`);
+  return $tweet;
+}
+
+const renderTweets = function(tweetData) {
+  $('#tweets-container').empty();
+  for (let tweet of tweetData) {
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+}
