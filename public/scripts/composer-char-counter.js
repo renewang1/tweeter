c1c71d6295757079d00edabf98766306a4718f5c
Rene Wang <rene.wang1@hotmail.com>
2020-12-20 21:34:05 +0000

    removed commented out code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $('#tweet-text').on('input', function() {
-    // let counterValue = $(this).siblings().children('.counter').get(0).value - 1;
-    // console.log(counterValue)
-    // $(this).siblings().children('.counter').get(0).value = 140 - this.value.length;
-
     const charLength = $(this).val().length;
     const $tweetForm = $(this).closest("form");
     const $counter = $tweetForm.find(".counter");
